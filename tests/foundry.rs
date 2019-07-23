adfadfe679e729d30974a04603fc3e7ca6c683cb
Martin Larralde <martin.larralde@ens-paris-saclay.fr>
2019-07-23 12:08:52 +0000

    Skip long-running OBO foundry tests to prevent CI timeout

--- tests/foundry.rs
+++ tests/foundry.rs
@@ -54,8 +54,8 @@ macro_rules! foundrytest {
     )
 }
 
-foundrytest!(go(go, "go.json"));
-foundrytest!(go_basic(go, "go/go-basic.json"));
-foundrytest!(go_plus(go, "go/extensions/go-plus.json"));
+foundrytest!(#[ignore] go(go, "go.json"));
+foundrytest!(#[ignore] go_basic(go, "go/go-basic.json"));
+foundrytest!(#[ignore] go_plus(go, "go/extensions/go-plus.json"));
 foundrytest!(pato(pato, "pato.json"));
-foundrytest!(mondo(mondo, "mondo.json"));
+foundrytest!(#[ignore] mondo(mondo, "mondo.json"));
