44dbc538bb0e29249b514cfa27241917e44fb9c1
Martin Larralde <martin.larralde@ens-paris-saclay.fr>
2019-08-04 23:36:16 +0000

    Add more conversion tests for header clauses

--- tests/convert/mod.rs
+++ tests/convert/mod.rs
@@ -1,3 +1,6 @@
+#[macro_use]
+extern crate pretty_assertions;
+
 extern crate fastobo_graphs;
 extern crate serde_json;
 extern crate serde_yaml;
@@ -9,29 +12,75 @@ use fastobo_graphs::FromGraph;
 use fastobo_graphs::model::Graph;
 use fastobo_graphs::model::GraphDocument;
 
-pub mod into_graph {
-    use super::*;
+macro_rules! test_impl {
+    ($($name:ident,)*) => (
+        pub mod into_graph {
+            use super::*;
 
-    macro_rules! test_impl {
-        ($case:ident) => {
-            #[test]
-            #[allow(non_snake_case)]
-            fn $case() {
-                let basename = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
-                    .join("tests")
-                    .join("convert")
-                    .join(stringify!($case));
-                let obofile = basename.with_extension("obo");
-                let jsonfile = File::open(basename.with_extension("json")).unwrap();
+            macro_rules! test_impl {
+                ($case:ident) => {
+                    #[test]
+                    #[ignore]
+                    #[allow(non_snake_case)]
+                    fn $case() {
+                        let basename = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+                            .join("tests")
+                            .join("convert")
+                            .join(stringify!($case));
+                        let obofile = basename.with_extension("obo");
+                        let jsonfile = File::open(basename.with_extension("json")).unwrap();
 
-                let obo = fastobo::ast::OboDoc::from_file(obofile).unwrap();
-                let expected: GraphDocument = serde_json::from_reader(jsonfile).unwrap();
-                let actual = GraphDocument::from(Graph::from(obo));
+                        let obo = fastobo::ast::OboDoc::from_file(obofile).unwrap();
+                        let expected: GraphDocument = serde_json::from_reader(jsonfile).unwrap();
+                        let actual = GraphDocument::from(Graph::from(obo));
 
-                assert_eq!(expected, actual, "graphs do not match!")
+                        assert_eq!(expected, actual, "graphs do not match!")
+                    }
+                }
             }
-        }
-    }
 
-    test_impl!(header_saved_by);
+            $(test_impl!($name);)*
+        }
+
+        pub mod from_graph {
+            use super::*;
+
+            macro_rules! test_impl {
+                ($case:ident) => {
+                    #[test]
+                    #[ignore]
+                    #[allow(non_snake_case)]
+                    fn $case() {
+                        let basename = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+                            .join("tests")
+                            .join("convert")
+                            .join(stringify!($case));
+                        let obofile = basename.with_extension("obo");
+                        let jsonfile = File::open(basename.with_extension("json")).unwrap();
+
+                        let expected = fastobo::ast::OboDoc::from_file(obofile).unwrap();
+                        let doc: GraphDocument = serde_json::from_reader(jsonfile).unwrap();
+                        let graph = doc.graphs.into_iter().next().unwrap();
+                        let actual = fastobo::ast::OboDoc::from_graph(graph).unwrap();
+
+                        assert_eq!(expected, actual, "graphs do not match!")
+                    }
+                }
+            }
+
+            $(test_impl!($name);)*
+        }
+    )
 }
+
+
+test_impl!(
+    header_data_version,
+    header_default_namespace,
+    header_format_version,
+    header_namespace_id_rule,
+    header_remark,
+    header_saved_by,
+    header_subsetdef,
+    header_synonymtypdef,
+);
